31a121f00e5bc71ed731d28c58c36a444e232535
kelingm <lingmei.ke@crgecent.com>
2021-01-26 12:53:14 +0800

    temp:  临时提交

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,16 +1,16 @@
 import _ from 'lodash';
 
-import { printMe } from './print.js';
-import './styles.scss';
-import message from './message.js';
+import { printMe } from './print';
+// import './styles.scss';
+import message from './message';
 
 message();
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -29,7 +29,7 @@ document.body.appendChild(element);
 
 if (module.hot) {
   // 针对这个文件的热更新，就不会使用浏览器刷新，而是局部更新
-  module.hot.accept('./print.js', function () {
+  module.hot.accept('./print.js', () => {
     // 当 print.js 内部发生变更时可以告诉 webpack 接受更新的模块。
     console.log('Accepting the updated printMe module!');
     document.body.removeChild(element);
